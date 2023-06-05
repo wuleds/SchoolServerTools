25a3a12857d2b177e090e01eb1e60b0ccd7f7d44
wule <wl1030ds@gmali.com>
2023-06-06 01:51:09 +0800

    1

---
+++ src/main/java/hjnu/wl/SchoolServerTools/controller/RegisterCon.java
@@ -0,0 +1,12 @@
+package hjnu.wl.SchoolServerTools.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author wule
+ */
+@RequestMapping("/register")
+public class RegisterCon
+{
+
+}
---
+++ src/main/java/hjnu/wl/SchoolServerTools/controller/SigninCon.java
@@ -0,0 +1,12 @@
+package hjnu.wl.SchoolServerTools.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author wule
+ */
+@RequestMapping("/signin")
+public class SigninCon
+{
+
+}
