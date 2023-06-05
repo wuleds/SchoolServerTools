a33953def31de9d7e07d581d75c55a57b6647d45
wule <wl1030ds@gmali.com>
2023-06-06 01:43:06 +0800

    Initial commit

---
+++ src/main/java/hjnu/wl/SchoolServerTools/SchoolServerToolsApplication.java
@@ -0,0 +1,13 @@
+package hjnu.wl.SchoolServerTools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolServerToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolServerToolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hjnu/wl/SchoolServerTools/SchoolServerToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package hjnu.wl.SchoolServerTools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolServerToolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
