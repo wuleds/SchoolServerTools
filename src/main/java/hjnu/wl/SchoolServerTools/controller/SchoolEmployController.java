80e6c443b610d454cbc3b6389ce8d44dd652b939
wule <wl1030ds@gmali.com>
2023-06-08 00:25:55 +0800

    9

--- src/main/java/hjnu/wl/SchoolServerTools/controller/SchoolEmployController.java
+++ src/main/java/hjnu/wl/SchoolServerTools/controller/SchoolEmployController.java
@@ -2,6 +2,8 @@ package hjnu.wl.SchoolServerTools.controller;
 //汉江师范学院 数计学院 吴乐创建于2023/6/8 0:20
 
 import hjnu.wl.SchoolServerTools.model.SchoolEmploy;
+import hjnu.wl.SchoolServerTools.service.SchoolEmployService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,6 +12,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/schoolEmploy")
 public class SchoolEmployController
 {
+    private static SchoolEmployService schoolEmployService;
+    @Autowired
+    private void setSchoolEmployService(SchoolEmployService schoolEmployService)
+    {
+        SchoolEmployController.schoolEmployService = schoolEmployService;
+    }
+
     @RequestMapping("/release")
     @ResponseBody
     public String releaseSchoolEmploy(SchoolEmploy schoolEmploy)
